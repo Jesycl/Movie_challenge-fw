c5b3967555c5103c2e081cb6f6fe07c25af525c4
Jesycl <142137601+Jesycl@users.noreply.github.com>
2024-01-30 21:51:57 -0600

    Information was copied to the readme

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,22 @@ import "./App.css";
 import PersonList from "./PersonList";
 import { PersonProps } from "./PersonCard"
 
+
+function App() {
+ 
+    return (
+      <div className="App">
+          <h1> Movie Challenge </h1>
+        <PersonList apiResponse={initialApiResponse}/>
+      
+      </div>
+     
+    );
+    
+  }
+  
+  export default App;
+
 interface ApiResponse {
   page: number;
   results: PersonProps[];
@@ -411,48 +427,4 @@ const initialApiResponse: ApiResponse = {
 }
 
 
-function App() {
- 
-  return (
-    <div className="App">
-        <h1> Movie Challenge </h1>
-      <PersonList apiResponse={initialApiResponse}/>
-    
-    </div>
-   
-  );
-  
-}
 
-export default App;
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
